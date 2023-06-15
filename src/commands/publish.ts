093985b36f07c7d581e174549a558e9303ee37e1
LanzoInc <olanzovc@gmail.com>
2023-06-15 14:40:13 -0400

    feat: publish will use symlinks and is more optimized

--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import fs from "fs";
 import tar from "tar";
-import tmp from "tmp";
 
 import { exec, execSync } from "child_process";
 import { Listr } from "listr2";
@@ -19,6 +18,7 @@ interface PublishContext {
 
 interface publishactionoptions {
     noScripts?: boolean,
+    packer?: "pnpm"|"yarn"|"npm"
     // overwrite?: boolean,
     // push?: boolean,
     // migrate?: boolean,
@@ -40,7 +40,6 @@ export async function publishaction(packagePath:string,options:publishactionopti
                     if(!readPackageFile.name){
                         throw new Error(`Package is missing the "name" field.`)
                     }
-                    // const { Orginization, Package } = extractPackageName(readPackageFile.name);
                     const extractedInfo = extractPackageName(readPackageFile.name);
 
                     ctx.packageinfo = {
@@ -51,24 +50,6 @@ export async function publishaction(packagePath:string,options:publishactionopti
                     await createHomeFolder();
                 }
             },
-            // {
-            //     title: "Running prepublishOnly scripts",
-            //     skip: options.noScripts,
-            //     task: (ctx,task) => {
-            //         const Scripts = ctx.readPackageFile["scripts"];
-            //         if(Scripts){
-            //             const prepublishOnly = Scripts["prepublishOnly"];
-            //             if(prepublishOnly){
-            //                 const res = execSync(prepublishOnly);
-            //                 task.title = task.title + " ==>> " + res.toString();
-            //             }else{
-            //                 task.skip(`No "prepublishOnly" script found`)
-            //             }
-            //         }else{
-            //             task.skip(`No scripts field in package file`)
-            //         }
-            //     }
-            // },
             {
                 title: "Running lpm:prepublishOnly scripts",
                 skip: options.noScripts,
@@ -77,8 +58,25 @@ export async function publishaction(packagePath:string,options:publishactionopti
                     if(Scripts){
                         const prepublishlocalpm = Scripts["lpm:prepublishOnly"];
                         if(prepublishlocalpm){
-                            const res = execSync(prepublishlocalpm);
-                            task.title = task.title + " ==>> " + res.toString();
+                            return new Promise<void>((resolve,reject) => {
+                                const p = exec(prepublishlocalpm)
+                                p.stdout.on("data", (data)=>{
+                                    task.output = data
+                                    console.log(data)
+                                })
+                                p.stderr.on("data", (data)=>{
+                                    task.output = data
+                                    console.log(data)
+                                })
+                                p.on("close", exitCode => {
+                                    if(exitCode === 1){
+                                        reject("lpm:prepublishOnly script failed")
+                                    }
+                                    resolve();
+                                })
+                            })
+                            // const res = execSync(prepublishlocalpm,{stdio: "inherit", cwd: packagePath});
+                            // task.title = task.title + " ==>> " + res.toString();
                         }else{
                             task.skip(`No "lpm:prepublishOnly" script found`)
                         }
@@ -88,120 +86,95 @@ export async function publishaction(packagePath:string,options:publishactionopti
                 }
             },
             {
-                title: "Packaging...",
+                title: "Package & Publish",
                 task: async (ctx, task) => {
-                    await new Promise<void>((resolve,reject) => {
-                        tmp.dir( {prefix: "localpm"}, async (err:string | undefined,temppath:string,cleanupcb:()=>void) => {
-                            if(err){
-                                throw err;
-                            }
-                            
-                            ctx.tempFolderCleanupfunc = cleanupcb;
-                            const npmpackexec = exec(`npm pack --pack-destination=${temppath}`,{cwd: packagePath})
-                            npmpackexec.on("exit", async (code)=>{
-                                if(code === 1) {
-                                    const err = `Something went wrong when packing with npm. exited with code :: ${code}`
-                                    console.log(err)
-                                    reject(`Something went wrong when packing with npm. ::${code}`)
-                                }
-                                const pkgtgz = (await fs.promises.readdir(temppath).catch(e=>{
-                                    reject(`Could not access tarbal ${e}`)
-                                }))[0];
-                                
-                                const localpmPkgsPath = getlocalpmPkgs();
-                                //check for the orginization in localpm pkgs. localpm/pkgs/@orginization
-                                const orgNameInlocalpm = ctx.packageinfo.Orginization
-                                const orgPathInlocalpm = path.join(localpmPkgsPath, orgNameInlocalpm);
 
-                                try{
-                                    fs.mkdirSync(orgPathInlocalpm, {recursive: true})
-                                }catch (e) {
-                                    if(e.code !== "EEXIST"){
-                                        throw e;
-                                    }
-                                }
-                                    await fs.promises.mkdir(orgPathInlocalpm).catch(e => {
-                                        if(e.code !== "EEXIST"){
-                                            throw e;
-                                        }
-                                    });
-   
-
-                                //check for the package in localpm pkgs within the given orginization. localpm/pkgs/@orginization/packagename
-                                const packageInOrgPath = path.join(orgPathInlocalpm,ctx.packageinfo.Package);
-                                if(!fs.existsSync(packageInOrgPath)){
-                                    await fs.promises.mkdir(packageInOrgPath).catch(e => {
-                                        if(e.code !== "EEXIST"){
-                                            throw e;
-                                        }
-                                    });
-                                }
- 
-                               //We use packageInOrgPath instead of binding to a specific @version since we're using symlinks now.
-                                await fs.promises.mkdir(packageInOrgPath).catch(e => {
-                                    if(e.code !== "EEXIST"){
-                                        throw e;
-                                    }
-                                });
-
-                                const localpmPackagesJson:localpmPackages = JSON.parse(await fs.promises.readFile(await getlocalpmPackageJsonPath(true),"utf8").catch(e=>{throw e}));
-                                const packageNameMap = ctx.packageinfo.Name;
-                                // localpmPackagesJson.packages[packageNameMap] = localpmPackagesJson.packages[packageNameMap] || {};
-                                
-                                //add to localpm-packages.json
-                                Object.assign(localpmPackagesJson.packages, {
-                                    [packageNameMap]: {
-                                        // retain old installations if exists
-                                        installations: localpmPackagesJson.packages[packageNameMap] && 
-                                        localpmPackagesJson.packages[packageNameMap]["installations"]
-                                        || [],
-                                        //We use packageInOrgPath instead of binding to a specific @version since we're using symlinks now.
-                                        resolve: path.join(packageInOrgPath,"package"),
-                                    }
-                                })
-                            
-                                await fs.promises.writeFile(await getlocalpmPackageJsonPath(),JSON.stringify(localpmPackagesJson,null,2)).catch(e => {
-                                    throw e;
-                                })
-
-                                //unpack tarbal
-                                tar.x(
-                                    {
-                                        file: path.join(temppath,pkgtgz),
-                                        //We use packageInOrgPath instead of binding to a specific @version since we're using symlinks now.
-                                        cwd: packageInOrgPath,
-                                        sync: true,
-                                    }
-                                )
-   
-                                cleanupcb();
-                                ctx.tempFolderCleanupfunc = undefined;
-                                resolve();
-                            })
-                        })
-                    });
-                },
-                rollback: async (ctx): Promise<void> => {
-                    if(ctx.tempFolderCleanupfunc){
-                        ctx.tempFolderCleanupfunc();
+                    task.output = "running pack program...";
+                    //use pnpm to pack, npm is slow and yarn does not ignore files like "!**/*.tsbuildinfo" in the package files field.
+                    execSync(`${options.packer} pack`, { cwd: packagePath, stdio: "ignore" });
+                    const temppath = packagePath //since we no longer use temp dir, just point to package path, just incase we switch back in future
+                    task.output = `packed gzip. ${temppath}`;
+                    const localpmPkgsPath = getlocalpmPkgs();
+                    const orgNameInlocalpm = ctx.packageinfo.Orginization || "@"; //Write files that aren't part of an orginization to a default @/ directory
+                    const orgPathInlocalpm = path.join(localpmPkgsPath, orgNameInlocalpm);
+                    task.output = `Writing path ${orgPathInlocalpm}...`;
+                    try {
+                        fs.mkdirSync(orgPathInlocalpm, { recursive: true });
                     }
+                    catch (e) {
+                        if (e.code !== "EEXIST") {
+                            throw e;
+                        }
+                    }
+                    //We use packageInOrgPath instead of binding to a specific @version since we're using symlinks now.
+                    const packageInOrgPath = path.join(orgPathInlocalpm, ctx.packageinfo.Package);
+                    await fs.promises.mkdir(packageInOrgPath).catch(e => {
+                        if (e.code !== "EEXIST") {
+                            throw e;
+                        }
+                    });
+                    const localpmPackagesJson = JSON.parse(await fs.promises.readFile(await getlocalpmPackageJsonPath(true), "utf8").catch(e => { throw e; }));
+                    const packageNameMap = ctx.packageinfo.Name;
+                    //add to localpm-packages.json
+                    task.output = "Assigning to global packages...";
+                    Object.assign(localpmPackagesJson.packages, {
+                        [packageNameMap]: {
+                            // retain old installations if exists
+                            installations: localpmPackagesJson.packages[packageNameMap] &&
+                                localpmPackagesJson.packages[packageNameMap]["installations"]
+                                || [],
+                            //We use packageInOrgPath instead of binding to a specific @version since we're using symlinks now.
+                            resolve: path.join(packageInOrgPath, "package"),
+                        }
+                    });
+                    task.output = `Updating global packages...`;
+                    await fs.promises.writeFile(await getlocalpmPackageJsonPath(), JSON.stringify(localpmPackagesJson, null, 2)).catch(e => {
+                        throw e;
+                    });
+                    //unpack tarbal
+                    task.output = `Extracting tarbal...`;
+                    const tarbalName = (ctx.packageinfo.Orginization && (ctx.packageinfo.Orginization).replace("@","")+"-"+ctx.packageinfo.Package || ctx.packageinfo.Package)+`-${options.packer==="yarn"?"v":""}`+ctx.packageinfo.Version+".tgz";
+                    const tarbalOutDirectory = path.join(temppath,tarbalName)
+                    //remove any published package from the folder . packageName.package
+                    try{
+                        fs.rmSync(path.join(packageInOrgPath,"package"),{ recursive: true });
+                    }catch(e) {
+                        if(e.code !== "ENOENT"){
+                            console.log(`You may need to manually remove this directory: ${packageInOrgPath}`)
+                            throw e;
+                        }
+                    };
+
+                    tar.x({
+                        file: tarbalOutDirectory,
+                        cwd: packageInOrgPath,
+                        sync: true,
+
+                    });
+                    //remove tgz file
+                    try{
+                        fs.rmSync(tarbalOutDirectory,{ recursive: true })
+                    }catch(e) {
+                        if(e.code !== "ENOENT"){
+                            console.log(`You may need to manually remove this directory: ${tarbalOutDirectory}`)
+                            throw e;
+                        }
+                    };
                 }
-            },    
+            }
         ]
-    )
-    await MT.run().catch(e => {
+        )
+    await MT.run().then((ctx) => {
+        console.log("published", ctx.packageinfo.Name+" v"+ctx.packageinfo.Version);
+    }).catch(e => {
         // console.log(e);
-    }).then(ctx => {
-        console.log("published ", (ctx as PublishContext).packageinfo.Name);
-        // if(options.push){
-        //     pushaction(packagePath,{publish: false})
-        // }
     })
 }
 
 export default function publish(program: typeof CommanderProgram){
     program.command("publish")
     .option("--noScripts", "Do not execute any scripts", false)
+    .option("--packer [string]", "What package manager to use to pack. npm,yarn,pnpm", "pnpm")
     // .option("--overwrite", "Overwrites any existing version without asking for permission.", false)
     // .option("--push", "Push changes after publishing.", false)
     // .option("--migrate", "Makes other version installations migrate to this release", false)
